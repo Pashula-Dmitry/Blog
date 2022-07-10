6125e0a19d216c3cfabc74f9933f2606b3a82148
Dmitry Pashula <pashula.dmitro@lanars.com>
2022-07-10 14:24:06 +0300

    Create Api post ina image

---
+++ controllers/PostController.js
@@ -0,0 +1,141 @@
+import PostModal from '../models/Post.js';
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModal({
+            title: req.body.title,
+            text: req.body.text,
+            tags: req.body.tags,
+            user: req.userId,
+            imageUrl: req.body.imageUrl,
+        });
+
+        const post = await doc.save();
+
+        res.json(post);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: 'Post creation is failed',
+        });
+    }
+};
+
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModal.find().populate('user').exec();
+        res.json(posts);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: 'Posts weren\'t received',
+        });
+    }
+};
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        PostModal.findOneAndUpdate(
+            {
+            _id: postId
+
+        },
+            {
+            $inc: { viewsCount: 1 }
+        },
+            {
+                returnDocument: 'after',
+            },
+            (err, doc) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        message: 'Post weren\'t received',
+                    });
+                }
+
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'Post hasn\'t found',
+                    });
+                }
+
+                res.json(doc);
+            }
+        ).populate('user');
+
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: 'Post weren\'t received',
+        });
+    }
+};
+
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        PostModal.findOneAndDelete(
+            {
+                _id: postId
+
+            },
+            (err, doc) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        message: 'Failed to delete article',
+                    });
+                }
+
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'article is not found',
+                    });
+                }
+
+                res.json({
+                    success: true,
+                });
+            }
+        );
+
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: 'Post weren\'t received',
+        });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModal.updateOne(
+            {
+            _id: postId
+        }, {
+                title: req.body.title,
+                text: req.body.text,
+                tags: req.body.tags,
+                user: req.userId,
+                imageUrl: req.body.imageUrl,
+            })
+
+        res.json({
+            success: true,
+        });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: 'Failed update an article',
+        });
+    }
+};
